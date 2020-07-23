6bf4ff3c21adf035bf23ca9e040557430af2f681
Camille <camille.coutens@gmail.com>
2020-07-23 14:51:42 +0200

    loader

--- pages/work/index.vue
+++ pages/work/index.vue
@@ -1,4 +1,12 @@
 <template>
+<div>
+  <div v-if="$apollo.queries.albums.loading">
+    <div class="loader">
+      <div class="inner one"></div>
+      <div class="inner two"></div>
+      <div class="inner three"></div>
+    </div>
+  </div>
   <div v-if="!$apollo.queries.albums.loading" class="w_bloc_albums">
     <div class="w_albums" v-for="album in albums.nodes" :key="album.id">
       <nuxt-link :to="'/work/'+ album.slug">
@@ -9,6 +17,7 @@
       </nuxt-link>
     </div>
   </div>
+</div>
 </template>
 
 <script>
@@ -45,6 +54,72 @@ import albums from '~/queries/getAlbums.gql';
         padding-top: 1.042vw;
       }
     }
+  }
+}
+
+.loader {
+  position: absolute;
+  top: calc(50% - 32px);
+  left: calc(50% - 32px);
+  width: 34px;
+  height: 34px;
+  border-radius: 50%;
+  perspective: 800px;
+}
+
+.inner {
+  position: absolute;
+  box-sizing: border-box;
+  width: 100%;
+  height: 100%;
+  border-radius: 50%;  
+}
+
+.inner.one {
+  left: 0%;
+  top: 0%;
+  animation: rotate-one 1s linear infinite;
+  border-bottom: 3px solid #000;
+}
+
+.inner.two {
+  right: 0%;
+  top: 0%;
+  animation: rotate-two 1s linear infinite;
+  border-right: 3px solid #000;
+}
+
+.inner.three {
+  right: 0%;
+  bottom: 0%;
+  animation: rotate-three 1s linear infinite;
+  border-top: 3px solid #000;
+}
+
+@keyframes rotate-one {
+  0% {
+    transform: rotateX(35deg) rotateY(-45deg) rotateZ(0deg);
+  }
+  100% {
+    transform: rotateX(35deg) rotateY(-45deg) rotateZ(360deg);
+  }
+}
+
+@keyframes rotate-two {
+  0% {
+    transform: rotateX(50deg) rotateY(10deg) rotateZ(0deg);
+  }
+  100% {
+    transform: rotateX(50deg) rotateY(10deg) rotateZ(360deg);
+  }
+}
+
+@keyframes rotate-three {
+  0% {
+    transform: rotateX(35deg) rotateY(55deg) rotateZ(0deg);
+  }
+  100% {
+    transform: rotateX(35deg) rotateY(55deg) rotateZ(360deg);
   }
 }
 
