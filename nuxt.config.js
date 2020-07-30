f359fbd49efa442cef6111aab137106c60261208
Camille <camille.coutens@gmail.com>
2020-07-30 16:40:36 +0200

    loading bar

--- nuxt.config.js
+++ nuxt.config.js
@@ -30,7 +30,10 @@ export default {
   /*
   ** Customize the progress-bar color
   */
-  loading: { color: '#fff' },
+  loading: {
+    color: '#000',
+    height: '2px'
+  },
   /*
   ** Global CSS
   */
